469d79547983eba9ca5cbcffa96cf9373eef1364
Imad Saad <em4d2002@gmail.com>
2023-03-18 17:00:39 +0400

    test

--- Makefile
+++ Makefile
@@ -1,9 +1,11 @@
-build :	
-		docker compose --env-file .env build
 
 run :
-		docker compose --env-file=./.env up --build
+	mkdir -p /Users/isaad/data/wp
+	mkdir -p /Users/isaad/data/sql
+	docker compose -f ./docker-compose.yml up --build
 
+build :	
+		docker compose build
 bgrun :
 		docker-compose build && docker-compose up -d
 
@@ -12,3 +14,8 @@ stop :
 
 stopv :
 		docker-compose down -v
+
+clean :
+		rm -rf /Users/isaad/data/wp
+		rm -rf /Users/isaad/data/sql
+		docker system prune -f -a
--- wordpress/tools/test.sh
+++ wordpress/tools/test.sh
@@ -6,11 +6,12 @@ then
   sed -i "s/username_here/$WORDPRESS_DB_USER/g" /var/www/html/wp-config.php
   sed -i "s/password_here/$WORDPRESS_DB_PASSWORD/g" /var/www/html/wp-config.php
   sed -i "s/localhost/$WORDPRESS_DB_HOST/g" /var/www/html/wp-config.php
+  cat wp-config.php
 
 
   # install wordpress with wp-cli
   sudo -u www-data wp core install --url=$WEBSITE_URI --title=$TITLE --admin_user=$WORDPRESS_ADMIN --admin_password=$WORDPRESS_ADMIN_PASSWORD --admin_email=$WORDPRESS_ADMIN_EMAIL
   sudo -u www-data wp user create ${WORDPRESS_USER} ${WORDPRESS_USER_EMAIL} --user_pass=${WORDPRESS_USER_PASSWORD} --role=author
-  sudo -u www-data wp theme install raft --activate
+  sudo -u www-data wp theme install inspiro --activate
 
 fi
